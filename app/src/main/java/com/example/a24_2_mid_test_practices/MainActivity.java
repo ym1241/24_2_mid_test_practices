962f309b860ab21e9608eb7ccdb1de930050547d
YM Kim <m0m32929@naver.com>
2024-10-08 15:54:01 +0900

    예제9-1

--- app/src/main/java/com/example/a24_2_mid_test_practices/MainActivity.java
+++ app/src/main/java/com/example/a24_2_mid_test_practices/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.a24_2_mid_test_practices;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,12 +22,60 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(new MyGraphicView(this));
+    }
+    private static class MyGraphicView extends View{
+        public MyGraphicView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(@NonNull Canvas canvas) {
+            super.onDraw(canvas);
+            Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setColor(Color.GREEN);
+            canvas.drawLine(20, 20, 600, 20, paint);
+
+            paint.setColor(Color.BLUE);
+            paint.setStrokeWidth(10);
+            canvas.drawLine(20, 60, 600, 60, paint);
+
+            paint.setColor(Color.RED);
+            paint.setStrokeWidth(0);
+
+            paint.setStyle(Paint.Style.FILL);
+            Rect rect1 = new Rect(20, 100, 20 + 200, 100 + 200);
+            canvas.drawRect(rect1, paint);
+
+            paint.setStyle(Paint.Style.STROKE);
+            Rect rect2 = new Rect(260, 100, 260 + 200, 100 + 200);
+            canvas.drawRect(rect2, paint);
+
+            RectF rect3 = new RectF(500, 100, 500 + 200, 100 + 200);
+            canvas.drawRoundRect(rect3, 40, 40, paint);
+
+            canvas.drawCircle(120, 440, 100, paint);
+
+            paint.setStrokeWidth(10);
+            Path path1 = new Path();
+            path1.moveTo(20, 580);
+            path1.lineTo(20 + 100, 580 + 100);
+            path1.lineTo(20 + 200, 580);
+            path1.lineTo(20 + 300, 580 + 100);
+            path1.lineTo(20 + 400, 580);
+            canvas.drawPath(path1, paint);
+
+            paint.setStrokeWidth(0);
+            paint.setTextSize(60);
+            canvas.drawText("안드로이드", 20, 780, paint);
+            //Paint.setStrokeCap()
+            //Canvas.drawOval()
+            //Paint.setColor(Color.argb())
+            paint.setColor(Color.BLACK);
+            paint.setStyle(Paint.Style.FILL);
+            RectF rect4 = new RectF(20, 800, 20 + 500, 800 + 120);
+            canvas.drawOval(rect4, paint);
+        }
     }
 }
